3d98c439ae74d87b64be2e0777cf7923a05f535d
cobaltclaudia <ceruleancee@gmail.com>
2022-01-01 21:33:38 -0600

    Update main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 mod print;
 mod vars;
+mod string;
+mod tuples;
+mod arrays;
+mod vectors;
+mod conditionals;
 
 fn main() {
     print::print_funct();
     vars::vars_funct();
+    string::run();
+    tuples::run();
+    arrays::run();
+    vectors::run();
+    conditionals::run();
 }
