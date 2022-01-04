0e1f3d7e6404490b0f6be4efcf68f6fd1fc1059a
cobaltclaudia <ceruleancee@gmail.com>
2022-01-03 22:21:20 -0600

    Add functions.rs

---
+++ src/functions.rs
@@ -0,0 +1,19 @@
+pub fn run(){
+    greetings("Hello", "y'all");
+
+    let get_sum = add(5,5);
+    println!("sum: {}", get_sum);
+
+
+    let n3: i32 = 10;
+    let add_nums = |n1: i32, n2:i32| n1 +n2 + n3;
+    println!("C sum: {}", add_nums(3,3));
+}
+
+fn greetings(greet: &str, name: &str){
+    println!("{} {}, nice to know you", greet, name );
+}
+
+fn add(n1: i32, n2: i32) -> i32{
+    n1 + n2
+}
