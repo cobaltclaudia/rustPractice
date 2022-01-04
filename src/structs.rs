4f60120b657f4787f8beae0063bb28d19b43e5da
cobaltclaudia <ceruleancee@gmail.com>
2022-01-03 22:22:03 -0600

    Add structs.rs

---
+++ src/structs.rs
@@ -0,0 +1,68 @@
+// traditional
+// struct Color {
+//     red: u8,
+//     green: u8,
+//     blue:u8,
+// }
+
+//tuple
+// struct Color(u8, u8, u8);
+
+struct Person{
+    first_name: String,
+    last_name: String
+}
+
+impl Person{
+    fn new (first: &str, last: &str) -> Person {
+        Person {
+            first_name: first.to_string(),
+            last_name: last.to_string()
+        }
+    }
+
+    fn full_name(&self) -> String {
+    format!("{} {}", self.first_name, self.last_name)
+}
+
+    fn set_last_name(&mut self, last: &str) {
+        self.last_name = last.to_string();
+    }
+
+    fn to_tuple(self) -> (String, String) {
+        // no semicolon means a return
+        (self.first_name, self.last_name)
+    }
+
+}
+
+
+
+pub fn run(){
+    // let mut c = Color {
+    //     red: 255,
+    //     green: 0,
+    //     blue: 0,
+    //
+    // };
+
+    // c.blue = 200;
+    // println!("Color: {} {} {}", c.red, c.green, c.blue);
+
+
+    // let mut c = Color(255,0,0);
+    //
+    // c.0 = 200;
+    // println!("Color: {} {} {}", c.0, c.1, c.2);
+
+
+    let mut p = Person::new("Mary", "Doe");
+    println!("Person {} {}", p.first_name, p.last_name);
+    p.set_last_name("somethingElse");
+    println!("Person {} ", p.full_name());
+
+    println!("person tuple {:?}", p.to_tuple());
+
+
+}
+
