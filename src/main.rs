01a4f805c76b631eba642a649e1a498c6ff5579a
cobaltclaudia <ceruleancee@gmail.com>
2022-05-25 18:54:48 -0500

    Remove snake.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
-mod snake;
+mod print;
+mod vars;
+mod string;
+mod tuples;
+mod arrays;
+mod vectors;
+mod conditionals;
+mod loops;
+mod functions;
+mod pointers_ref;
+mod structs;
+mod enums;
+mod cli;
 
 fn main() {
-    snake::snake_game();
+    print::print_funct();
+    vars::vars_funct();
+    string::run();
+    tuples::run();
+    arrays::run();
+    vectors::run();
+    conditionals::run();
+    loops::run();
+    functions::run();
+    pointers_ref::run();
+    structs::run();
+    enums::run();
+    cli::run();
 }
